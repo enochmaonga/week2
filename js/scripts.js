fe77e6b1a91be52aa7e031359f4af50836468f45
enochmaonga <enoch.mokua@student.moringaschool.com>
2021-11-11 14:56:17 +0300

    adding conditionals

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,25 @@ var value1Input = prompt('enter a value');
 var value1 = parseInt(value1Input);
 alert(value1Input + ' was a ' + typeof  value1Input + ' and was converted and saved as a new variable, the new data type of value1 is '+ typeof  value1)
 
+var value2Input = prompt('enter a value');
+var value2 = parseInt(value2Input);
 
+alert(value2Input + ' was a ' + typeof  value2Input + ' and was converted and saved as a new variable, the new data type of value2 is '+ typeof  value2)
 
-
-
-
-
-
+//addition, subtraction, divisin and multiplication
+var operationsValue = prompt("which operation do you want to perform?(addition || +, subtraction, division, multiplication)");
+if (operationsValue === 'add') {
+    alert(inPutValue  + ' the sum of '+ value1 + ' and ' +value2 + ' is '+ addTwoNumbers(value1,value2));
+}
+else if(operationsValue === 'divide') {
+    alert(inPutValue  + ' the division of '+ value1 + ' and ' +value2 + ' is '+ divideTwoNumbers(value1,value2));
+}
+else if(operationsValue === 'subtract') {
+    alert(inPutValue  + ' the difference of '+ value1 + ' and ' +value2 + ' is '+ subtractTwoNumbers(value1,value2));
+}
+else{
+    alert(inPutValue  + ' the multiplication of '+ value1 + ' and ' +value2 + ' is '+ multiplyTwoNumbers(value1,value2));}
+    
 
 
 
@@ -42,9 +54,9 @@ function addTwoNumbers(c, d) {
     }
   console.log(subtractTwoNumbers(45,20));
 
-  function multiplyThreeNumbers(c, d, g) {
-    return c * d * g
+  function multiplyTwoNumbers(c, d) {
+    return c * d
     }
-  console.log(multiplyThreeNumbers(7,9,2));
+  console.log(multiplyTwoNumbers(7,9));
 
 
